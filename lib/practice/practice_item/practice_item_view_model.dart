9d2293511ec5eaaad2b9e10f77db3d7009f6196a
Filipe Santiago <porto.santiago@hotmail.com>
2022-09-12 17:08:48 -0300

    update edit flow

--- lib/practice/practice_item/practice_item_view_model.dart
+++ lib/practice/practice_item/practice_item_view_model.dart
@@ -4,7 +4,9 @@ import 'package:practice_maniac/infra/router/navigator.dart';
 import 'package:practice_maniac/practice/domain/practice.dart';
 import 'package:practice_maniac/practice/domain/practices.dart';
 import 'package:practice_maniac/practice/practice_routes.dart';
+import 'package:practice_maniac/utils/defined.dart';
 import 'package:rx_command/rx_command.dart';
+import 'package:rxdart/rxdart.dart';
 
 @injectable
 class PracticeItemViewModel extends ViewModelData<Practice> {
@@ -16,17 +18,22 @@ class PracticeItemViewModel extends ViewModelData<Practice> {
 
   PracticeItemViewModel(this.practices, this.navigator) {
     remove = RxCommand.createFromStream((_) => _onRemove());
-    edit = RxCommand.createSyncNoParamNoResult(_onEdit);
+    edit = RxCommand.createFromStream((_) => _onEdit());
   }
 
   Stream<void> _onRemove() {
     return practices.remove(model.value);
   }
 
-  void _onEdit() {
-    navigator.go(
-      PracticeRoutes().detail,
-      arguments: model.value,
-    );
+  Stream<void> _onEdit() {
+    return navigator
+        .go<Practice>(
+          PracticeRoutes().detail,
+          arguments: model.value,
+        )
+        .map((practice) => Defined(practice))
+        .where((practice) => practice.exist())
+        .doOnData((practice) => model.value.edit(practice.get()))
+        .switchMap((_) => model.value.update());
   }
 }
